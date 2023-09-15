71ee2943c54b5a6af26d1eb190d47dde1d3a36c6
THECODERjONE <86699083+THECODERjONE@users.noreply.github.com>
2023-09-15 18:26:18 +0200

    Create whichvaluesdoesthewhileloopshow.js

---
+++ JavaScript-ECMAScript/javascript info tasks/Which values does the while loop show/whichvaluesdoesthewhileloopshow.js
@@ -0,0 +1,12 @@
+For every loop iteration, write down which value it outputs and then compare it with the solution.
+
+Both loops alert the same values, or not?
+
+The prefix form ++i:
+
+let i = 0;
+while (++i < 5) alert( i );
+The postfix form i++
+
+let i = 0;
+while (i++ < 5) alert( i );
