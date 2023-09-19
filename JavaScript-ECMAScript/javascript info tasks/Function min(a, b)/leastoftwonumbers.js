8b4d504a9c6fd8b14330a405051f9c0847c99162
THECODERjONE <86699083+THECODERjONE@users.noreply.github.com>
2023-09-19 16:52:28 +0200

    This is the core of everything
    Now we are heating up. According to my coder colleges, this is one of the cornerstones.

---
+++ JavaScript-ECMAScript/javascript info tasks/Function min(a, b)/leastoftwonumbers.js
@@ -0,0 +1,22 @@
+// Write a function min(a,b) which returns the least of two numbers a and b.
+
+// For instance:
+
+function min(a, b) {
+    return a < b ? a : b;
+}
+
+// function min(a, b) {
+//     if (a < b) {
+//       return a;
+//     } else {
+//       return b;
+//     }
+//   }
+
+//Provided by page
+
+min(2, 5) == 2
+min(3, -1) == -1
+min(1, 1) == 1
+
---
+++ JavaScript-ECMAScript/javascript info tasks/Function pow(x,n)/powxn.js
@@ -0,0 +1,26 @@
+// Write a function pow(x,n) that returns x in power n.
+//Or, in other words, multiplies x by itself n times and returns the result.
+
+// pow(3, 2) = 3 * 3 = 9
+// pow(3, 3) = 3 * 3 * 3 = 27
+// pow(1, 100) = 1 * 1 * ...* 1 = 1
+// Create a web-page that prompts for x and n, and then shows the result of pow(x,n).
+
+function pow(x, n) {
+    let result = x;
+  
+    for (let i = 1; i < n; i++) {
+      result *= x;
+    }
+  
+    return result;
+  }
+  
+  let x = prompt("Whats the base you want to elevate?", '');
+  let n = prompt("Whats the potency you want to elevate the base to?", '');
+  
+  if (n < 1) {
+    alert(`Power ${n} is not supported, use a positive integer`);
+  } else {
+    alert( pow(x, n) );
+  }
