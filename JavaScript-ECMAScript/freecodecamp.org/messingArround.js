8df8afb05b24cdc8e69cd72744e47250b968afbd
THECODERjONE <86699083+THECODERjONE@users.noreply.github.com>
2023-09-24 04:58:22 +0200

    Move folder + add course
    Organized a bit, changed some names + added the new course I started from freecodecamp.org

---
+++ JavaScript-ECMAScript/freecodecamp.org/messingArround.js
@@ -0,0 +1,13 @@
+// Im at 47% of the course and just now I realized I sould be feeding GitHub with some of my practice.
+
+// Return a Value from a Function with Return:
+
+while (false) {
+    function timesFive(n) {
+        return alert(n * 5);
+    }
+    timesFive(5);
+    timesFive(2);
+    timesFive(0);
+}
+
