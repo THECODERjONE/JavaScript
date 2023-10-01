1fb54dbce14e0daf9e3ac120bf0d7ba6fe4469a4
THECODERjONE <86699083+THECODERjONE@users.noreply.github.com>
2023-10-01 16:09:00 +0200

    Adding some context

--- JavaScript-ECMAScript/freecodecamp.org/basicJavaScriptPractices.js
+++ JavaScript-ECMAScript/freecodecamp.org/basicJavaScriptPractices.js
@@ -1,3 +1,12 @@
+/*
+JavaScript is a scripting language you can use to make web pages interactive.
+It is one of the core technologies of the web, along with HTML and CSS, and is supported by all modern browsers.
+
+In this course, you'll learn fundamental programming concepts in JavaScript.
+You'll start with basic data structures like numbers and strings.
+Then you'll learn to work with arrays, objects, functions, loops, if/else statements, and more.
+*/
+
 // Im at 47% of the course and just now I realized I sould be feeding GitHub with some of my practice.
 
 // Note that I am adding only the result i give at the page, I am not only copying te instructions of the exercise but my answer inside it aswell.
--- JavaScript-ECMAScript/freecodecamp.org/debugging.js
+++ JavaScript-ECMAScript/freecodecamp.org/debugging.js
@@ -1,3 +1,12 @@
+/*
+Debugging is the process of going through your code, finding any issues, and fixing them.
+
+Issues in code generally come in three forms: syntax errors that prevent your program from running,
+runtime errors where your code has unexpected behavior, or logical errors where your code doesn't do what you intended.
+
+In this course, you'll learn how to use the JavaScript console to debug programs and prevent common issues before they happen.
+*/
+
 // Use the JavaScript Console to Check the Value of a Variable
 
 // Use the console.log() method to print the value of the variable a where noted in the code.
