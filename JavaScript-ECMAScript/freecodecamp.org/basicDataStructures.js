2e46493862e7579efa3a67280f283663835169f9
THECODERjONE <86699083+THECODERjONE@users.noreply.github.com>
2023-10-01 17:27:09 +0200

    Create basicDataStructures.js

---
+++ JavaScript-ECMAScript/freecodecamp.org/basicDataStructures.js
@@ -0,0 +1,14 @@
+// Data can be stored and accessed in many ways. You already know some common JavaScript data structures — arrays and objects.
+
+// In this Basic Data Structures course, you'll learn more about the differences between arrays and objects,
+// and which to use in different situations. You'll also learn how to use helpful JS methods like splice() and Object.keys() to access
+// and manipulate data.
+
+// Use an Array to Store a Collection of Data
+
+// We have defined a variable called yourArray. Complete the statement by assigning an array of at least 5 elements
+//  in length to the yourArray variable. Your array should contain at least one string, one number, and one boolean.
+
+while (false) {
+    
+}
