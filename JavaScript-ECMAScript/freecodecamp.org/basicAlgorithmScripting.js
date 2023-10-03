9f4ea707a7d9ecdb81d5bc0e01bcc10e1d807dd6
THECODERjONE <86699083+THECODERjONE@users.noreply.github.com>
2023-10-03 20:06:44 +0200

    Update basicAlgorithmScripting.js

--- JavaScript-ECMAScript/freecodecamp.org/basicAlgorithmScripting.js
+++ JavaScript-ECMAScript/freecodecamp.org/basicAlgorithmScripting.js
@@ -141,5 +141,27 @@ But for the purpose of this challenge, we would like you to use one of the JavaS
 */
 
 while (false) {
+  function confirmEnding(str, target) {
+    let strL = str.length;
+    let targetL = target.length;
+    let auxStr = '';
+    for (let i = strL -1; i >= (strL - targetL);
+    i--) {
+      auxStr = str[i] + auxStr;
+    }
+    return (auxStr === target);
+  }
+  
+  confirmEnding("Bastian", "n");
+}
+
+// Repeat a String Repeat a String
+
+/*
+Repeat a given string str (first argument) for num times (second argument). Return an empty string if num is not a positive number.
+For the purpose of this challenge, do not use the built-in .repeat() method.
+*/
+
+while (false) {
   
 }
