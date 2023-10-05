a1b777e8cd1a59df01ede5d1a68d4ae47d1d7af1
THECODERjONE <86699083+THECODERjONE@users.noreply.github.com>
2023-10-05 21:50:10 +0200

    nearly finished OOP

--- JavaScript-ECMAScript/freecodecamp.org/oop.js
+++ JavaScript-ECMAScript/freecodecamp.org/oop.js
@@ -190,5 +190,267 @@ return true if the candidate is a Dog, otherwise return false.
 */
 
 while (false) {
-    
+  function Dog(name) {
+    this.name = name;
+  }
+  
+  // Only change code below this line
+  function joinDogFraternity(candidate) {
+  return candidate.constructor === Dog;
+  }
+}
+
+// Change the Prototype to a New Object
+
+// Add the property numLegs and the two methods eat() and describe() to the prototype of Dog by setting the prototype to a new object.
+
+while (false) {
+  function Dog(name) {
+    this.name = name;
+  }
+  
+  Dog.prototype = {
+    // Only change code below this line
+  numLegs: 4,
+  eat: function() {
+    console.log('nom nom nom');
+  },
+  describe: function() {
+    console.log('My name is' + this.name);
+  }
+  };
+}
+
+// Remember to Set the Constructor Property when Changing the Prototype
+
+// Define the constructor property on the Dog prototype.
+
+while (false) {
+  function Dog(name) {
+    this.name = name;
+  }
+  
+  // Only change code below this line
+  Dog.prototype = {
+    constructor: Dog,
+    numLegs: 4,
+    eat: function() {
+      console.log("nom nom nom");
+    },
+    describe: function() {
+      console.log("My name is " + this.name);
+    }
+  };
+}
+
+// Understand Where an Object’s Prototype Comes From
+
+// Use isPrototypeOf to check the prototype of beagle.
+
+while (false) {
+  function Dog(name) {
+    this.name = name;
+  }
+  
+  let beagle = new Dog("Snoopy");
+  
+  // Only change code below this line
+  
+  Dog.prototype.isPrototypeOf(beagle);
+}
+
+// Understand the Prototype Chain
+
+// Modify the code to show the correct prototype chain.
+
+while (false) {
+  function Dog(name) {
+    this.name = name;
+  }
+  
+  let beagle = new Dog("Snoopy");
+  
+  Dog.prototype.isPrototypeOf(beagle);  // yields true
+  
+  // Fix the code below so that it evaluates to true
+  Object.prototype.isPrototypeOf(Dog.prototype);
+}
+
+// Use Inheritance So You Don't Repeat Yourself
+
+// The eat method is repeated in both Cat and Bear. Edit the code in the spirit of DRY by moving the eat method to the Animal supertype.
+
+while (false) {
+  function Cat(name) {
+    this.name = name;
+  }
+  
+  Cat.prototype = {
+    constructor: Cat,
+  };
+  
+  function Bear(name) {
+    this.name = name;
+  }
+  
+  Bear.prototype = {
+    constructor: Bear,
+  };
+  
+  function Animal() { }
+  
+  Animal.prototype = {
+    constructor: Animal,
+    eat: function() {
+      console.log("nom nom nom");
+    }
+  };
+}
+
+// Inherit Behaviors from a Supertype
+
+// Use Object.create to make two instances of Animal named duck and beagle.
+
+while (false) {
+  function Animal() { }
+
+Animal.prototype = {
+  constructor: Animal,
+  eat: function() {
+    console.log("nom nom nom");
+  }
+};
+
+// Only change code below this line
+
+let duck = Object.create(Animal.prototype); // Change this line
+let beagle = Object.create(Animal.prototype); // Change this line
+}
+
+// Set the Child's Prototype to an Instance of the Parent
+
+// Modify the code so that instances of Dog inherit from Animal.
+
+while (false) {
+  function Animal() { }
+
+Animal.prototype = {
+  constructor: Animal,
+  eat: function() {
+    console.log("nom nom nom");
+  }
+};
+
+function Dog() { }
+
+// Only change code below this line
+
+Dog.prototype = Object.create(Animal.prototype);
+let beagle = new Dog();
+}
+
+// Reset an Inherited Constructor Property
+
+// Fix the code so duck.constructor and beagle.constructor return their respective constructors.
+
+while (false) {
+  function Animal() { }
+  function Bird() { }
+  function Dog() { }
+  
+  Bird.prototype = Object.create(Animal.prototype);
+  Dog.prototype = Object.create(Animal.prototype);
+  
+  // Only change code below this line
+  
+  Bird.prototype.constructor = Bird;
+  Dog.prototype.constructor = Dog;
+  
+  let duck = new Bird();
+  let beagle = new Dog();  
+}
+
+// Add Methods After Inheritance
+
+/*
+Add all necessary code so the Dog object inherits from Animal and the Dog's prototype constructor is set to Dog.
+Then add a bark() method to the Dog object so that beagle can both eat() and bark(). The bark() method should print Woof! to the console.
+*/
+
+while (false) {
+  function Animal() { }
+Animal.prototype.eat = function() { console.log("nom nom nom"); };
+
+function Dog() { }
+
+// Only change code below this line
+Dog.prototype = Object.create(Animal.prototype);
+Dog.prototype.constructor = Dog;
+Dog.prototype.bark = function() {console.log('Woof!')};
+// Only change code above this line
+
+let beagle = new Dog();
+}
+
+// Override Inherited Methods
+
+// Override the fly() method for Penguin so that it returns the string Alas, this is a flightless bird.
+
+while (false) {
+  function Bird() { }
+
+Bird.prototype.fly = function() { return "I am flying!"; };
+
+function Penguin() { }
+Penguin.prototype = Object.create(Bird.prototype);
+Penguin.prototype.constructor = Penguin;
+
+// Only change code below this line
+
+Penguin.prototype.fly = function(){
+  return 'Alas, this is a flightless bird.';
+}
+
+// Only change code above this line
+
+let penguin = new Penguin();
+console.log(penguin.fly());
+}
+
+// Use a Mixin to Add Common Behavior Between Unrelated Objects
+
+// Create a mixin named glideMixin that defines a method named glide. Then use the glideMixin to give both bird and boat the ability to glide.
+
+while (false) {
+  let bird = {
+    name: "Donald",
+    numLegs: 2
+  };
+  
+  let boat = {
+    name: "Warrior",
+    type: "race-boat"
+  };
+  
+  // Only change code below this line
+  
+  let glideMixin = function(object) {
+    object.glide = function() {
+      console.log(object.type + ' is gliding!');
+    }
+  };
+  
+  glideMixin(boat);
+  glideMixin(bird);
+  
+  bird.glide();
+}
+
+// Use Closure to Protect Properties Within an Object from Being Modified Externally
+
+// Change how weight is declared in the Bird function so it is a private variable.
+// Then, create a method getWeight that returns the value of weight 15.
+
+while (false) {
+  
 }
